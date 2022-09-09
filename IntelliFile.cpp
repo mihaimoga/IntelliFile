efbe4f870bf0d91da8fd21f6564fef4969f3b84e
Stefan-Mihai MOGA <contact@emvs.site>
2022-09-09 20:30:35 +0300

    Added Contributors hyperlink to AboutBox dialog; current version is 1.07

--- IntelliFile.cpp
+++ IntelliFile.cpp
@@ -188,6 +188,7 @@ protected:
 	CVersionInfo m_pVersionInfo;
 	CHyperlinkStatic m_ctrlWebsite;
 	CHyperlinkStatic m_ctrlEmail;
+	CHyperlinkStatic m_ctrlContributors;
 
 	DECLARE_MESSAGE_MAP()
 };
@@ -203,6 +204,7 @@ void CAboutDlg::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_WARNING, m_ctrlWarning);
 	DDX_Control(pDX, IDC_WEBSITE, m_ctrlWebsite);
 	DDX_Control(pDX, IDC_EMAIL, m_ctrlEmail);
+	DDX_Control(pDX, IDC_CONTRIBUTORS, m_ctrlContributors);
 }
 
 BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
@@ -251,6 +253,7 @@ BOOL CAboutDlg::OnInitDialog()
 
 	m_ctrlWebsite.SetHyperlink(_T("https://www.emvs.site/"));
 	m_ctrlEmail.SetHyperlink(_T("mailto:contact@emvs.site"));
+	m_ctrlContributors.SetHyperlink(_T("https://github.com/mihaimoga/IntelliFile/graphs/contributors"));
 
 	return TRUE;  // return TRUE unless you set the focus to a control
 	// EXCEPTION: OCX Property Pages should return FALSE
---
+++ README.md
@@ -0,0 +1,7 @@
+[IntelliFile](https://www.emvs.site/intellifile/) is an alternative Windows version to the famous Total Commander!
+
+This application is developed in Microsoft Visual C++, and it is licensed under GNU General Public License v3.0.
+
+This open source application has a Setup project to be deployed on Microsoft Windows 10+ machines.
+
+Contributions are welcome. Be mindful of our [Contribution Rules](CONTRIBUTING.md) to increase the likelihood of your contribution getting accepted.

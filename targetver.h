7e6b5e15fe251586ccd2d740002236bdea3afad5
Stefan-Mihai Moga <stefan-mihai.moga@nxp.com>
2023-02-18 14:13:30 +0200

    Updated copyright header

--- targetver.h
+++ targetver.h
@@ -1,3 +1,16 @@
+/* This file is part of IntelliFile application developed by Mihai MOGA.
+
+IntelliFile is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Open
+Source Initiative, either version 3 of the License, or any later version.
+
+IntelliFile is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+IntelliFile. If not, see <http://www.opensource.org/licenses/gpl-3.0.html>*/
+
 // This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface
 // (the "Fluent UI") and is provided only as referential material to supplement the
 // Microsoft Foundation Classes Reference and related electronic documentation
